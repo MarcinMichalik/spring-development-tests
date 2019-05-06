24c8297605873400a1c85797d1f30a24ca0950e2
marcinm <marcinmat16@gmail.com>
2019-05-06 12:15:41 +0200

    init

---
+++ src/main/java/me/michalik/springdevelopment/SpringDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package me.michalik.springdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/michalik/springdevelopment/SpringDevelopmentApplicationTests.java
@@ -0,0 +1,16 @@
+package me.michalik.springdevelopment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDevelopmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
